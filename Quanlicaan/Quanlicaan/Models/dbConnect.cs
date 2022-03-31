854d3e8acfa50b0373b1b4aec0963fb3b33ca570
tqsang1403 <sangtranhd20@gmail.com>
2022-03-31 17:57:09 +0700

    ss

---
+++ Quanlicaan/Quanlicaan/Models/dbConnect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Quanlicaan.Models
+{
+    public class dbConnect
+    {
+        public static void Connect()
+        {
+           SqlConnection conn = new SqlConnection(@"data source=TQS1403\SQLEXPRESS02;initial catalog=QuanLiCaAn;Integrated Security=True");
+            
+        }
+
+    }
+}
