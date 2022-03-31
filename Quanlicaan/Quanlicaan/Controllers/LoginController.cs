37bab6fb3e6cdbf615febb2e0e98ae757407ec3d
tqsang1403 <sangtranhd20@gmail.com>
2022-03-31 16:10:11 +0700

    đăng nhập, full option nhân viên, full option, phòng ban.

--- Quanlicaan/Quanlicaan/Controllers/LoginController.cs
+++ Quanlicaan/Quanlicaan/Controllers/LoginController.cs
@@ -1,31 +1,45 @@
-﻿using System.Web.Mvc;
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Mvc;
 
 namespace Quanlicaan.Controllers
 {
     public class LoginController : Controller
     {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Model2"].ConnectionString);
         // GET: Login
         [HttpGet]
-        public ActionResult Index()
+        public ActionResult Index(string thongBao)
         {
+            ViewBag.thongBao = thongBao;
             return View();
         }
+        [HttpPost]
+        public ActionResult DangNhapHeThong(string username, string password)
+        {
+            //kiem tra ten dang nhap va mat khau ok
+            string sqlQuery = "select 1 from NhanVien where username ='" + username + "' and upassword = '" + password + "'";
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Index(LoginModel model)
-        //{
+            SqlCommand cmd = new SqlCommand(sqlQuery, con);
+            cmd.Connection = con;
 
-        //    if( ModelState.IsValid)
-        //    {
-        //        SessionHelper.SetSession(new UserSession() { username = model.username });
-        //        return RedirectToAction("Index", "Home");
-        //    }
-        //    else
-        //    {
-        //        ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không đúng!");
-        //    }
-        //    return View(model);
-        //}
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                Response.Write("<script>alert('Login sucess')</script>");
+                Session["username"] = username;
+                return Redirect("/Home/Home");
+
+
+            }
+           
+            con.Close();
+            Response.Write("<script>alert('Login fail')</script>");
+            return RedirectToAction("Index", new { thongBao = "tên đăng nhập và mật khẩu không đúng" });
+        }
+
+    
     }
 }
--- Quanlicaan/Quanlicaan/Controllers/PhongBanController.cs
+++ Quanlicaan/Quanlicaan/Controllers/PhongBanController.cs
@@ -1,9 +1,5 @@
-﻿using Quanlicaan.DataAccess;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Quanlicaan.Controllers
