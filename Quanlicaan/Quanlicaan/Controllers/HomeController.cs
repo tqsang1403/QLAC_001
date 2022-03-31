b289c40e654d1c5fcc19fada51cb94cddac57d42
dangnb <dangnb@esct.vn>
2022-03-31 14:08:53 +0700

    chức năng đăng nhập

--- Quanlicaan/Quanlicaan/Controllers/HomeController.cs
+++ Quanlicaan/Quanlicaan/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Quanlicaan.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Home()
+        public ActionResult Index()
         {
             return View();
         }
--- Quanlicaan/Quanlicaan/Controllers/LoginController.cs
+++ Quanlicaan/Quanlicaan/Controllers/LoginController.cs
@@ -1,16 +1,51 @@
 ﻿using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 
 namespace Quanlicaan.Controllers
+
 {
+
     public class LoginController : Controller
     {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Model2"].ConnectionString);
         // GET: Login
         [HttpGet]
-        public ActionResult Index()
+        public ActionResult Index(string thongBao)
         {
+            ViewBag.thongBao = thongBao;
             return View();
         }
+        [HttpPost]
+        public ActionResult DangNhapHeThong(string username, string password)
+        {
+            //kiem tra ten dang nhap va mat khau ok
+            string sqlQuery = "select 1 from NhanVien where username ='" + username + "' and upassword = '" + password + "'";
 
+            SqlCommand cmd = new SqlCommand(sqlQuery, con);
+            cmd.Connection = con;
+
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                Response.Write("<script>alert('Login sucess')</script>");
+                Session["username"] = username;
+                return Redirect("/Home/Index");
+
+
+            }
+            //if (username == "root" && password == "123")
+            //{
+            //    // chuyen huong sang trang login tai khoan 
+
+            //}
+            con.Close();
+            Response.Write("<script>alert('Login fail')</script>");
+            return RedirectToAction("Index", new { thongBao = "tên đăng nhập và mật khẩu không đúng" });
+        }
         //[HttpPost]
         //[ValidateAntiForgeryToken]
         //public ActionResult Index(LoginModel model)
