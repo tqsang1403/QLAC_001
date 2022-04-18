f6c98360027888f2ae49b33669536aec85a5d028
dangnb <dangnb@esct.vn>
2022-04-18 15:09:41 +0700

    viết view model cho đăng kí ca ăn cá nhân

---
+++ Quanlicaan/Quanlicaan/Models/DangKyCaNhanModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quanlicaan.Models
+{
+    public class DangKyCaNhanModel
+    {
+        public string hoTen { get; set; }   
+        public string phongBan { get; set; }
+        public DateTime ngayDK { get; set; }
+        public int SLCa1 { get; set; }  
+        public int SLCa2 { get; set; }
+        public int SLCa3 { get; set; }
+        public DangKyCaNhanModel()
+        {
+            var info = (UserLoginModel)Session["UserSession"];
+        }
+    }
+}
